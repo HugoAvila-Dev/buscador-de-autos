2778384033e673624765148908e0fffde1136897
Hugo <hhugo.avila97@gmail.com>
2023-08-14 13:37:03 -0400

    filtrar autos por año

--- js/app.js
+++ js/app.js
@@ -38,11 +38,11 @@ document.addEventListener('DOMContentLoaded', () =>{
 //Event listener para los select de búsqueda
 marca.addEventListener('change', e => {
     datosBusqueda.marca = e.target.value;
-
     filtrarAuto();
 });
 year.addEventListener('change', e => {
-    datosBusqueda.year = e.target.value;
+    datosBusqueda.year = parseInt(e.target.value);
+    filtrarAuto();
 });
 minimo.addEventListener('change', e => {
     datosBusqueda.minimo = e.target.value;
@@ -86,14 +86,21 @@ function llenarSelect() {
 
 //Función que filtra en base a la búsqueda 
 function filtrarAuto() {
-    const resultado = autos.filter( filtrarMarca );
+    const resultado = autos.filter( filtrarMarca ).filter( filtrarYear );
     console.log(resultado)
 }
 
 function filtrarMarca(auto) {
     const { marca } = datosBusqueda;
     if( marca ){
-        return auto.marca === datosBusqueda.marca
+        return auto.marca === marca
+    }
+    return auto;
+}
+function filtrarYear(auto) {
+    const { year } = datosBusqueda;
+    if( year ){
+        return auto.year === year
     }
     return auto;
 }
