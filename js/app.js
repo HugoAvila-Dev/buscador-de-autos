922dd67fbe0012da4f85acb7b95b87811f53c94f
Hugo <hhugo.avila97@gmail.com>
2023-08-14 13:06:49 -0400

    leer el valor de la marca

--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
 //Variables
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+//contenedor para los resultados
+const resultado = document.querySelector('#resultado');
+
 
 const max = new Date().getFullYear();
 const min = max-10;
 
+//Generar un objeto con la búsqueda 
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: ''
+
+}
 
 
 //Eventos
@@ -15,8 +35,30 @@ document.addEventListener('DOMContentLoaded', () =>{
     //Llena las opciones de años
     llenarSelect();
 })
+//Event listener para los select de búsqueda
+marca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;
 
-
+    filtrarAuto();
+});
+year.addEventListener('change', e => {
+    datosBusqueda.year = e.target.value;
+});
+minimo.addEventListener('change', e => {
+    datosBusqueda.minimo = e.target.value;
+});
+maximo.addEventListener('change', e => {
+    datosBusqueda.maximo = e.target.value;
+});
+puertas.addEventListener('change', e => {
+    datosBusqueda.puertas = e.target.value;
+});
+transmision.addEventListener('change', e => {
+    datosBusqueda.transmision = e.target.value;
+})
+color.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value;
+})
 
 //Funciones
 function mostrarAutos() {
@@ -38,6 +80,19 @@ function llenarSelect() {
         const opcion = document.createElement('option');
         opcion.value = i;
         opcion.textContent = i;
-        year.appendChild(opcion);
+        year.appendChild(opcion); // Agrega las opciones de año al select
+    }
+}
+
+//Función que filtra en base a la búsqueda 
+function filtrarAuto() {
+    const resultado = autos.filter( filtrarMarca );
+    console.log(resultado)
+}
+
+function filtrarMarca(auto) {
+    const { marca } = datosBusqueda;
+    if( marca ){
+        return auto.marca === datosBusqueda.marca
     }
 }
